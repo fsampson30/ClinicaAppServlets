fcc6447ff6e307cf5670f7f320c3021ea464fc60
Flavio Sampson <71216861+fsampson30@users.noreply.github.com>
2020-09-13 13:15:27 -0300

    Servlet para inserção de agendamento

---
+++ Servlets/ServletInsereAgendamento.java
@@ -0,0 +1,61 @@
+package Servlets;
+
+import Controller.SQLInsere;
+import Model.Agendamento;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ServletInsereAgendamento", urlPatterns = {"/ServletInsereAgendamento"})
+
+public class ServletInsereAgendamento extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            int length = request.getContentLength();
+            byte[] input = new byte[length];
+            ServletInputStream sin = request.getInputStream();
+            int c, count = 0;
+            while ((c = sin.read(input, count, input.length - count)) != -1) {
+                count += c;
+            }
+            sin.close();
+
+            String parametro = new String(input);
+            String resposta;
+            parametro = URLDecoder.decode(parametro, "UTF-8");
+            Gson gson = new Gson();
+            Agendamento agendamento = new Agendamento();
+            agendamento = gson.fromJson(parametro, Agendamento.class);
+            
+            SQLInsere sqlInsere = new SQLInsere();
+            boolean ok = sqlInsere.insereAgendamento(agendamento);
+            if(ok){
+                resposta = "1";
+            } else{
+                resposta = "0";
+            }            
+            response.setStatus(HttpServletResponse.SC_OK);
+            OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+            writer.write(resposta);
+            writer.flush();
+            writer.close();                        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

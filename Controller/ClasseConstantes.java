64323d16f3f07e23a08a835b3df4423b7c1c7bcd
Flavio Sampson <71216861+fsampson30@users.noreply.github.com>
2020-09-13 12:44:02 -0300

    Classe com informações de conexão

---
+++ Controller/ClasseConstantes.java
@@ -0,0 +1,32 @@
+package Controller;
+
+public class ClasseConstantes {
+    
+    final private static String url = "jdbc:mysql://caminhodaaplicacao";
+    final private static String user = "usuario";
+    final private static String pass = "senha;
+    final private static String driver = "com.mysql.cj.jdbc.Driver";
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+
+    
+    
+    
+    
+    
+}
